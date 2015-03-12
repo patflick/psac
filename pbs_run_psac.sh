3f9de595f487b6f7194161a0b59a8a4e230a98d4
r4d2 <patrick.flick@gmail.com>
2015-03-11 21:23:04 -0500

    benchmarks on cyence

--- pbs_run_psac.sh
+++ pbs_run_psac.sh
@@ -20,13 +20,16 @@
 
 #PBS -lnodes=16:ppn=16:compute,walltime=1:00:00
 
+source /home/pflick/gcc48env.sh
+
 MPIRUN=/usr/mpi/gcc/mvapich2-1.7-qlc/bin/mpirun
 
 # Change to directory from which qsub command was issued 
 cd $PBS_O_WORKDIR
 
 # define executable to run
-EXE=./release/bin/test_sac
+#EXE=./release/bin/test_sac
+EXE=./release/bin/benchmark_sac
 
 echoerr() { cat <<< "$@" 1>&2; }
 
@@ -42,12 +45,13 @@ ln -s $NOW $OUT_FOLDER/newest
 INFILE=/lustre/alurugroup/pflick/human_g1k_v37.actg
 
 #for p in 1 2 4 8 16 32 64 128 256 512 1024
-for i in `seq 1 10`
-do
-	for p in 64 128 256 #512 1024
+#for i in `seq 1 10`
+#do
+	for p in 64 128  #128 256 #512 1024
 	do
-		echo "### Running with p = $p processors ###"
-		printf "$p;" 1>&2
-		/usr/bin/time -f "%E" $MPIRUN -np $p -errfile-pattern=$LOG_FOLDER/err-$p-%r.log -outfile-pattern=$LOG_FOLDER/out-$p-%r.log $EXE $INFILE
+		printf "### Running with p = $p processors ###" 1>&2
+		#printf "$p;" 1>&2
+		#/usr/bin/time -f "%E" $MPIRUN -np $p -errfile-pattern=$LOG_FOLDER/err-$p-%r.log -outfile-pattern=$LOG_FOLDER/out-$p-%r.log $EXE -f $INFILE -i 5
+		$MPIRUN -np $p $EXE -f $INFILE -i 5
 	done
-done
+#done
