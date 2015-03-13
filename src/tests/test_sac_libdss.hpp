e83da24d34b96ab9acab686b98318f95530d46a5
r4d2 <patrick.flick@gmail.com>
2015-03-12 20:02:35 -0500

    ldss 64

--- src/tests/test_sac_libdss.hpp
+++ src/tests/test_sac_libdss.hpp
@@ -14,24 +14,24 @@
 
 #include <mxx/collective.hpp>
 
-#include <divsufsort.h>
+#include <divsufsort64.h>
 
 #include "suffix_array.hpp"
 #include "timer.hpp"
 
 
 // C++ interface for libdivsufsort
-void divsufsort_sa_construction(const std::string& str, std::vector<saidx_t>& SA)
+void divsufsort_sa_construction(const std::string& str, std::vector<saidx64_t>& SA)
 {
-    saidx_t n = str.size();
+    saidx64_t n = str.size();
     std::basic_string<sauchar_t> T(reinterpret_cast<const sauchar_t*>(str.data()), n);
     SA.resize(n);
-    divsufsort(T.data(), &SA[0], n);
+    divsufsort64(T.data(), &SA[0], n);
 }
 
-bool divsufsort_sa_check(const std::string& str, const std::vector<saidx_t>& SA)
+bool divsufsort_sa_check(const std::string& str, const std::vector<saidx64_t>& SA)
 {
-    saidx_t n = str.size();
+    saidx64_t n = str.size();
     const sauchar_t* T = reinterpret_cast<const sauchar_t*>(str.data());
     return sufcheck(T, &SA[0], n, 1) == 0;
 }
@@ -46,22 +46,22 @@ bool test_compare_divsufsort_psac(std::string& str, MPI_Comm comm)
     timer t;
     double start = t.get_ms();
     //sa_construction_gl<saidx_t>(str, psac_SA, psac_ISA, comm);
-    suffix_array<std::string::iterator, saidx_t, false> sa(local_str.begin(), local_str.end(), comm);
+    //suffix_array<std::string::iterator, saidx_t, false> sa(local_str.begin(), local_str.end(), comm);
     //sa.construct_arr<2>(true);
-    sa.construct_arr<2>();
+    //sa.construct_arr<2>();
     double end = t.get_ms() - start;
 
     // get rank
     int rank;
     MPI_Comm_rank(comm, &rank);
 
-    std::vector<saidx_t> glSA = mxx::gather_vectors(sa.local_SA, comm);
+    //std::vector<saidx_t> glSA = mxx::gather_vectors(sa.local_SA, comm);
 
     if (rank == 0)
     {
         std::cerr << "PSAC Time: " << end << " ms" << std::endl;
         // run libdivsufsort
-        std::vector<saidx_t> dss_SA;
+        std::vector<saidx64_t> dss_SA;
         double start = t.get_ms();
         divsufsort_sa_construction(str, dss_SA);
         double end = t.get_ms() - start;
@@ -72,11 +72,11 @@ bool test_compare_divsufsort_psac(std::string& str, MPI_Comm comm)
         //std::cerr << "DSS: "; print_range(dss_SA.begin(), dss_SA.end());
         //std::cerr << "PSAC:"; print_range(psac_SA.begin(), psac_SA.end());
 
-        if(!divsufsort_sa_check(str, glSA))
-        {
-            std::cerr << "ERROR: wrong suffix array from PSAC" << std::endl;
-            return false;
-        }
+        //if(!divsufsort_sa_check(str, glSA))
+        //{
+        //    std::cerr << "ERROR: wrong suffix array from PSAC" << std::endl;
+        //    return false;
+        //}
         if(!divsufsort_sa_check(str, dss_SA))
         {
             std::cerr << "ERROR: wrong suffix array from libdivsufsort" << std::endl;
