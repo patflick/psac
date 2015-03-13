aeb9f4680af145820a2b32c167c2065828255638
r4d2 <patrick.flick@gmail.com>
2015-03-12 21:02:54 -0400

    plot and ldss 64 bit support

--- data/plot.R
+++ data/plot.R
@@ -1,9 +1,11 @@
 require(ggplot2)
 require(plyr)
 
-data <- read.csv("./benchmark_local_8.csv", sep=";", header=TRUE)
+#data <- read.csv("./benchmark_local_8.csv", sep=";", header=TRUE)
+data <- read.csv("./cyence_methods.csv", sep=";", header=TRUE)
 
-cdata <- ddply(data, c("p", "method"), summarise, time=mean(time))
+cdata <- ddply(data, c("p", "method"), summarise, time=mean(time)/1000)
 
-fig <- ggplot(cdata, aes(x=method, y=time)) + geom_bar()
+fig <- ggplot(cdata, aes(x=p, y=time, group=method, lty=method, color=method)) +
+        geom_line() + ylab("Time [s]")
 plot(fig)
