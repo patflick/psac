cd4d95773cf9f028eb69433e34554c1695b78c2a
r4d2 <patrick.flick@gmail.com>
2015-05-02 19:43:06 -0400

    thread safe and fully working custom reduction operators

--- include/mxx/reduction.hpp
+++ include/mxx/reduction.hpp
@@ -17,95 +17,161 @@
 #include <iterator>
 #include <limits>
 #include <functional>
+#include <mutex>
+#include <atomic>
 
 // mxx includes
 #include "datatypes.hpp"
 
 namespace mxx {
 
+/*
+ * Add any (key,value) pair to any MPI_Datatype using MPI caching (keyval)
+ * functionality (used internally to mxx for adding std::function objects to
+ * MPI datatypes, required for custom operators)
+ */
+template <typename K, typename T>
+class attr_map {
+private:
+    // static "variables" wrapped into static member functions
+    static int& key(){ static int k{0}; return k; }
+    static std::mutex& mut(){ static std::mutex m; return m; }
+    static std::map<K, int>& keymap(){ static std::map<K, int> m; return m; }
+
+    static int copy_attr(MPI_Datatype, int, void*, void *attribute_val_in, void *attribute_val_out, int *flag) {
+        T** out = (T**) attribute_val_out;
+        // copy via copy constructor
+        *out = new T(*(T*)(attribute_val_in));
+        *flag = 1;
+        return MPI_SUCCESS;
+    }
+
+    static int del_attr(MPI_Datatype, int, void *attribute_val, void *) {
+        delete (T*)attribute_val;
+        return MPI_SUCCESS;
+    }
+public:
+    static void set(const MPI_Datatype& dt, const K& k, const T& value) {
+        std::lock_guard<std::mutex> lock(mut());
+        if (keymap().find(k) == keymap().end()) {
+            // create new keyval with MPI
+            keymap()[k] = 0;
+            MPI_Type_create_keyval(&attr_map<K,T>::copy_attr,
+                                   &attr_map<K,T>::del_attr,
+                                   &(keymap()[k]), (void*)NULL);
+        }
+        // insert new key into keymap
+        // set the keyval pair
+        T* val = new T(value); // copy construct
+        MPI_Type_set_attr(dt, keymap()[k], (void*)val);
+    }
+    static T& get(const MPI_Datatype& dt, const K& k) {
+        std::lock_guard<std::mutex> lock(mut());
+        if (keymap().find(k) == keymap().end()) {
+            throw std::out_of_range("Key not mapped.");
+        }
+        int key = keymap()[k];
+        T* result;
+        int flag;
+        MPI_Type_get_attr(dt, key, &result, &flag);
+        if (!flag) {
+            throw std::out_of_range("Key not mapped.");
+        }
+        return *result;
+    }
+};
+
 /*********************************************************************
  *                     User supplied functions:                      *
  *********************************************************************/
 
+// Wraps custom operators. requires that the MPI_Datatype be duplicated
+// since it attaches the operator as a std::function to the MPI_Datatype
+// object.
+template <typename T, bool IsCommutative = true>
+class custom_op {
+public:
+    template <typename Func>
+    custom_op(Func& func, const MPI_Datatype& type) : builtin(false) {
+        // create user function
+        typedef std::function<void(void*,void*,int*)> func_t;
+        func_t user_func = [&func](void* invec, void* inoutvec, int* len) {
+            T* in = (T*) invec;
+            T* inout = (T*) inoutvec;
+            for (int i = 0; i < *len; ++i) {
+                inout[i] = func(in[i], inout[i]);
+            }
+        };
+        // attach function to the datatype
+        MPI_Type_dup(type, &type_copy);
+        attr_map<int, func_t>::set(type_copy, 1347, user_func);
 
-// this wrapper for user functions is not thread-safe!
-// TODO: use another approach in order to make this thread safe!
-// TODO: count template intializations, and make each of these static classes
-//       templated by a counter. thus each user function used in the code
-//       will get its own compile time template instantiation of this static
-//       class
-template <typename T>
-struct user_func_wrapper
-{
-    static bool active;
-    static std::function<T (T& x, T& y)> func;
-    static MPI_Datatype dt;
-    static void user_function(void *invec, void *inoutvec, int *len, MPI_Datatype *)
+        // create op
+        MPI_Op_create(&custom_op::user_function, IsCommutative, &op);
+    }
+
+    /* TODO: could potentially overload like the following for builtin
+     *       functions and types. this requires checking for both: builtin type
+     *       and builtin function
+     *
+    template <typename S>
+    custom_op(std::plus<S>&, const MPI_Datatype& type)
+        : builtin(true)
     {
-        if (!active) {
-          throw std::runtime_error("calling inactive user function");
-        }
-        // check in and out type
-        // TODO: check extend of all types!
+        std::cout << "use builtin function!" << std::endl;
+        type_copy = type;
+        op = MPI_SUM;
+    }
+    */
 
-        T* in = (T*) invec;
-        T* inout = (T*) inoutvec;
-        for (int i = 0; i < *len; ++i)
-        {
-            inout[i] = func(in[i], inout[i]);
+    /**
+     * @brief   Returns the MPI_Datatype which contains the std::function
+     *          reference.
+     */
+    MPI_Datatype get_type() const {
+        return type_copy;
+    }
+
+    /**
+     * @brief   Returns the MPI_Op operator for reduction operations.
+     */
+    MPI_Op get_op() const {
+        return op;
+    }
+
+    /// Destructor: cleanup MPI objects
+    virtual ~custom_op() {
+        if (!builtin) {
+            MPI_Op_free(&op);
+            MPI_Type_free(&type_copy);
         }
     }
+private:
+    // MPI custom Op function: (of type MPI_User_function)
+    static void user_function(void* in, void* inout, int* n, MPI_Datatype* dt) {
+        // get the std::function from the MPI_Datatype and call it
+        typedef std::function<void(void*,void*,int*)> func_t;
+        func_t f = attr_map<int, func_t>::get(*dt, 1347);
+        f(in, inout, n);
+    }
+
+    /// Whether the MPI_Op is a builtin operator (e.g. MPI_SUM)
+    bool builtin;
+    /// The copy (Type_dup) of the MPI_Datatype to work on
+    MPI_Datatype type_copy;
+    /// The MPI user operator
+    MPI_Op op;
 };
 
-template <typename T>
-std::function<T (T& x, T& y)> user_func_wrapper<T>::func;
-template <typename T>
-MPI_Datatype user_func_wrapper<T>::dt;
-template <typename T>
-bool user_func_wrapper<T>::active;
-
-// internal functions for creating the MPI_Op for user supplied operators
-template <typename T, typename Func, bool IsCommutative = true>
-MPI_Op create_user_op(Func& func) {
-    // get type
-    mxx::datatype<T> dt;
-    // define C++ wrapper for function pointers
-    typedef user_func_wrapper<T> wrapper;
-    if (wrapper::active) {
-      throw std::runtime_error("type wrapper still under use");
-    }
-    wrapper::active = true;
-    wrapper::func = std::move(func);
-    wrapper::dt = dt.type();
-
-    // create MPI user defined operation
-    MPI_Op op;
-    int commute = IsCommutative ? 1 : 0;
-    MPI_Op_create(&wrapper::user_function, commute, &op);
-    return op;
-}
-
-// internal function to free user supplied ops
-template <typename T>
-void free_user_op(MPI_Op op)
-{
-    typedef user_func_wrapper<T> wrapper;
-    if (!wrapper::active) {
-      throw std::runtime_error("type wrapper not active");
-    }
-    wrapper::active = false;
-    // cleanup
-    MPI_Op_free(&op);
-}
 
 /*********************************************************************
  *                Reductions                                         *
  *********************************************************************/
-// TODO: add more (vectorized, different reduce ops, etc)
+// TODO: add more (vectorized (std::vector, [begin,end),...), different reduce ops, etc)
 // TODO: naming of functions !?
+// TODO: come up with good naming scheme and standardize!
 // TODO: template specialize for std::min, std::max, std::plus, std::multiply
-//       etc for integers to use MPI builtin ops
-
+//       etc for integers to use MPI builtin ops?
 
 template <typename T>
 T allreduce(T& x, MPI_Comm comm = MPI_COMM_WORLD) {
@@ -128,15 +194,13 @@ T reduce(T& x, MPI_Comm comm = MPI_COMM_WORLD, int root = 0)
 
 template <typename T, typename Func>
 T allreduce(T& x, Func func, MPI_Comm comm = MPI_COMM_WORLD) {
-    // get user op
-    MPI_Op op = create_user_op<T, Func>(func);
     // get type
     mxx::datatype<T> dt;
+    // get custom op (and type for custom op)
+    mxx::custom_op<T> op(func, dt.type());
     // perform reduction
     T result;
-    MPI_Allreduce(&x, &result, 1, dt.type(), op, comm);
-    // clean up op
-    free_user_op<T>(op);
+    MPI_Allreduce(&x, &result, 1, op.get_type(), op.get_op(), comm);
     // return result
     return result;
 }
@@ -156,19 +220,17 @@ T exscan(T& x, MPI_Comm comm = MPI_COMM_WORLD) {
 
 template <typename T, typename Func>
 T exscan(T& x, Func func, MPI_Comm comm = MPI_COMM_WORLD) {
-    // get user op
-    MPI_Op op = create_user_op<T, Func>(func);
     // get type
     mxx::datatype<T> dt;
+    // get op
+    mxx::custom_op<T> op(func, dt.type());
     // perform reduction
     T result;
-    MPI_Exscan(&x, &result, 1, dt.type(), op, comm);
+    MPI_Exscan(&x, &result, 1, op.get_type(), op.get_op(), comm);
     int rank;
     MPI_Comm_rank(comm, &rank);
     if (rank == 0)
       result = T();
-    // clean up op
-    free_user_op<T>(op);
     return result;
 }
 
@@ -184,13 +246,15 @@ T scan(T& x, MPI_Comm comm = MPI_COMM_WORLD) {
 template <typename T, typename Func>
 T scan(T& x, Func func, MPI_Comm comm = MPI_COMM_WORLD) {
     // get user op
-    MPI_Op op = create_user_op<T, Func>(func);
+    //MPI_Op op = create_user_op<T, Func>(func);
     // get type
     mxx::datatype<T> dt;
+    // get op
+    mxx::custom_op<T> op(func, dt.type());
     T result;
-    MPI_Scan(&x, &result, 1, dt.type(), op, comm);
+    MPI_Scan(&x, &result, 1, op.get_type(), op.get_op(), comm);
     // clean up op
-    free_user_op<T>(op);
+    //free_user_op<T>(op);
     return result;
 }
 
@@ -198,7 +262,7 @@ T scan(T& x, Func func, MPI_Comm comm = MPI_COMM_WORLD) {
  *  reverse reductions (with reverse communicator)  *
  ****************************************************/
 
-void rev_comm(MPI_Comm comm, MPI_Comm& rev)
+inline void rev_comm(MPI_Comm comm, MPI_Comm& rev)
 {
     // get MPI parameters
     int rank;
@@ -254,24 +318,21 @@ T reverse_scan(T& x, Func func, MPI_Comm comm = MPI_COMM_WORLD) {
  ************************/
 // useful for testing global conditions, such as termination conditions
 
-template<int dummy = 0>
-bool test_all(bool x, MPI_Comm comm = MPI_COMM_WORLD) {
+inline bool test_all(bool x, MPI_Comm comm = MPI_COMM_WORLD) {
     int i = x ? 1 : 0;
     int result;
     MPI_Allreduce(&i, &result, 1, MPI_INT, MPI_LAND, comm);
     return result != 0;
 }
 
-template<int dummy = 0>
-bool test_any(bool x, MPI_Comm comm = MPI_COMM_WORLD) {
+inline bool test_any(bool x, MPI_Comm comm = MPI_COMM_WORLD) {
     int i = x ? 1 : 0;
     int result;
     MPI_Allreduce(&i, &result, 1, MPI_INT, MPI_LOR, comm);
     return result != 0;
 }
 
-template<int dummy = 0>
-bool test_none(bool x, MPI_Comm comm = MPI_COMM_WORLD) {
+inline bool test_none(bool x, MPI_Comm comm = MPI_COMM_WORLD) {
     int i = x ? 1 : 0;
     int result;
     MPI_Allreduce(&i, &result, 1, MPI_INT, MPI_LAND, comm);
