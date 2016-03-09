09c9993a87fcbed1ef8b357da245efe9a6e30d96
r4d2 <patrick.flick@gmail.com>
2016-03-08 20:21:54 -0600

    update mxx

--- pbs_run.sh
+++ pbs_run.sh
@@ -1,22 +1,27 @@
 #!/bin/sh
 
 # NOTE: just change the processor number here to something power of 2 plus 4
-#PBS -lnodes=36:ppn=16:compute,walltime=0:30:00
+
+#PBS -lnodes=72:ppn=16:compute,walltime=0:30:00
+NUM_VOTE_OFF=8
 
 #PBS -o BATCH_OUTPUT
 #PBS -e BATCH_ERRORS
 
 # set up compiler and MPI runtime environment
-source /home/pflick/gcc48env.sh
+#source /home/pflick/gcc48env.sh
+module load compilers/gcc-5.2
 MPIRUN=/usr/mpi/gcc/mvapich2-1.7-qlc/bin/mpirun
+
+BIN_FOLDER=./release/bin
 
 # Change to directory from which qsub command was issued
 cd $PBS_O_WORKDIR
 
 # define executable to run
 #EXE=./release/bin/benchmark_sac
-EXE=./release/bin/psac
-BM=./release/bin/benchmarks
+EXE=$BIN_FOLDER/psac
+BM=$BIN_FOLDER/mxx-bm-vote-off
 
 # prepare log folder
 OUT_FOLDER=runlog
@@ -42,13 +47,20 @@ MY_NODES=$(cat $PBS_NODEFILE | sort -u | tr '\n' ', ')
 echo "      $MY_NODES" >> $OUT_FOLDER/jobs.log
 echo ""
 
+# Old num nodes and PPN
 PPN=$PBS_NUM_PPN
 NUMNODES=$PBS_NUM_NODES
+NEW_NUMNODES=`expr $NUMNODES - $NUM_VOTE_OFF`
 NP=$(expr $NUMNODES \* $PPN)
-$MPIRUN -np $NP -errfile-pattern=$LOG_FOLDER/bm-err-$NP-%r.log -outfile-pattern=$LOG_FOLDER/bm-out-$NP-%r.log $BM
 
 #HOSTFILE=$PBS_NODEFILE
-HOSTFILE=blah.nodes
+NEW_HOSTFILE=new_nodefile_$NEW_NUMNODES.txt
+# run all-pairs bandwidth benchmark and exclude $NUM_VOTE_OFF worst nodes from the next job
+$MPIRUN -np $NP -errfile-pattern=$LOG_FOLDER/bm-err-$NP-%r.log -outfile-pattern=$LOG_FOLDER/bm-out-$NP-%r.log $BM $NUM_VOTE_OFF $NEW_HOSTFILE
+
+
+#HOSTFILE=$PBS_NODEFILE
+HOSTFILE=$NEW_HOSTFILE
 
 NUM_NODES=$(cat $HOSTFILE | wc -l)
 NP=$(expr $NUM_NODES \* $PPN)
